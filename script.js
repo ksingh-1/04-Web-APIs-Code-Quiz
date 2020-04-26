f1c1ff457e820ae115d94c4c1afe9eb92c4cee76
SSVF2 <kulpreet.s18@gmail.com>
2020-04-25 23:30:12 -0500

    updated style.css to script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+
+function initQuiz(){
+    let timeRemaining=0;
+}
+
+const startButtonE1=documentgetElementById("start-button");
+const timeRemainingE1=document.getElementById("time-remaining");
+const finalScoreE1=document.getElementById("final-score");
+const numQuestions=questions.length;
+const landContaninerE1=document.getElementById("landing-container");
+const quizContainerE1=document.getElementById("quiz-container");
+const finalContainerE1=document.getElementById("final-container");
+const submitButtonE1=document.getElementById("submit-initials");
+const highscoreContainerE1=document.getElementById("highscore-container");
+let highScores=[];
+
+}
+
