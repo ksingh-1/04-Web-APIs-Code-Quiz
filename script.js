210ebe744080018ce9abc199f6edda52038c5f32
SSVF2 <kulpreet.s18@gmail.com>
2020-05-02 21:56:20 -0500

    updating files

--- script.js
+++ script.js
@@ -1,18 +1,64 @@
 
-function initQuiz(){
-    let timeRemaining=0;
+$(document).ready(function() {
+    var counter=-1;
+    var userChoice= [];
+    var userArr=[];
+    var scores=100;
+
+    function initializeGame() {
+        $("#title").text("Coding Quiz");
+        counter=-1;
+        userChoice=[];
+        scores=100;
+        $("#main").show();
+        $("#back", "#clear").hide();
+    }
 }
 
-const startButtonE1=documentgetElementById("start-button");
-const timeRemainingE1=document.getElementById("time-remaining");
-const finalScoreE1=document.getElementById("final-score");
-const numQuestions=questions.length;
-const landContaninerE1=document.getElementById("landing-container");
-const quizContainerE1=document.getElementById("quiz-container");
-const finalContainerE1=document.getElementById("final-container");
-const submitButtonE1=document.getElementById("submit-initials");
-const highscoreContainerE1=document.getElementById("highscore-container");
-let highScores=[];
+$("#start-button").one("click", function() {
+    $("p, #start-button").hide();
+    $("#answers-button").show();
+    gameStart();
+    startTimer();
+});
 
+function gameStart() {
+    if (counter===questions.length) {
+        gameOver();
+    }else if (scores <=0) {
+        scores=0;
+        gameover();
+    }else {
+        counter++;
+        showQuestions();
+    }
 }
+    
 
+
+
+// Questions
+
+var questions = [
+    {
+        question: "What are if/else statements encapsulated by?",
+        answers: ["Brackets", "Quotation Marks", "Curly Brackets", "Parentheses"],
+        correct: "Parentheses"
+    },
+    {
+        question: "[0,1,2,3] represents a?",
+        answers: ["Series of Numbers", "Array", "Method", "Function"],
+        correct: "Array"
+    },
+    {
+        question: "the .md in the README.md stands for?",
+        answers: ["Made Directory", "Mark Down", "Mc Donalds", "Managed Dropdown"],
+        correct: "Mark Down"
+    },
+    {
+        question: "What does .js stand for?",
+        answers: ["Java Source", "Justified Source", "Justified Script", "Javascript"],
+        correct: "Javascript"
+    },
+
+]
